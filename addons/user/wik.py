f8c0a5fe0db0b53021e2eeb8662b5366962183d7
Danisxxx <cuentaregali178@gmail.com>
2024-12-17 22:45:01 -0400

    Eliminar claves de OpenAI y limpiar el repositorio

---
+++ addons/user/wik.py
@@ -0,0 +1,34 @@
+from configs._def_main_ import *
+import aiohttp
+from bs4 import BeautifulSoup
+
+@rex('wik')
+async def bot(client, message):
+    if len(message.command) < 2:
+        await message.reply_text("Por favor, proporciona un término de búsqueda.")
+        return
+    
+    query = " ".join(message.command[1:])
+    url = f"https://es.m.wikipedia.org/w/index.php?search={query}&title=Especial%3ABuscar&profile=advanced&fulltext=1&ns0=1&ns100=1&ns104=1"
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status != 200:
+                await message.reply_text("Hubo un problema al buscar en Wikipedia.")
+                return
+            html_content = await response.text()
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    result = soup.find('div', class_='mw-search-result-heading')
+    snippet = soup.find('div', class_='searchresult')
+
+    if not result or not snippet:
+        await message.reply_text("No se encontraron resultados.")
+        return
+
+    result_title = result.get_text(strip=True)
+    result_link = f"https://es.m.wikipedia.org{result.a['href']}"
+    snippet_text = snippet.get_text(strip=True)
+
+    formatted_result = ms.wik.format(busqueda=query, result=f"<a href='{result_link}'>{result_title}</a>", resultado=snippet_text[:1000])
+    await message.reply_text(formatted_result, disable_web_page_preview=True)
