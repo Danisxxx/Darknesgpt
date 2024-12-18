d922257ab4cd34391341163e1cf05d04c2788d66
Danisxxx <cuentaregali178@gmail.com>
2024-12-17 21:15:18 -0400

    Organizar archivos y subir al repositorio

--- addons/user/delca.py
+++ addons/user/delca.py
@@ -4,13 +4,11 @@ from configs._def_main_ import *
 
 @rex('delca')
 async def delca_callback(client, message):
-    # Enviar mensaje indicando que la caché está siendo eliminada
+
     sent_message = await message.reply("<b>Cache eliminando...</b>", reply_to_message_id=message.id)
 
-    # Directorio raíz del bot
     root_dir = os.path.dirname(os.path.abspath(__file__))
 
-    # Recorrer los subdirectorios para encontrar los __pycache__
     pycache_found = False
     for dirpath, dirnames, filenames in os.walk(root_dir):
         for filename in filenames:
@@ -19,14 +17,13 @@ async def delca_callback(client, message):
                 file_path = os.path.join(dirpath, filename)
                 try:
                     if os.path.isdir(file_path):
-                        shutil.rmtree(file_path)  # Eliminar directorios __pycache__
+                        shutil.rmtree(file_path) 
                     else:
-                        os.remove(file_path)  # Eliminar archivos .pyc
+                        os.remove(file_path)  
                 except Exception as e:
                     await sent_message.edit(f"<b>Error al eliminar cache: {e}</b>")
                     return
-
-    # Respuesta final
+                    
     if pycache_found:
         await sent_message.edit("<b>Cache eliminada correctamente.</b>")
     else:
