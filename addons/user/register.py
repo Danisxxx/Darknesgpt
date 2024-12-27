50093d5bfec3fb69fe76b985ee966717f32761a7
Danisxxx <cuentaregali178@gmail.com>
2024-12-27 00:24:23 -0400

    cambio 30

---
+++ addons/user/register.py
@@ -0,0 +1,36 @@
+from configs._def_main_ import *
+import mysql.connector
+from datetime import datetime
+
+@rex('register')
+async def register_user(message):
+    user_id = message.from_user.id
+    
+    connection = mysql.connector.connect(
+        host="mysql.railway.internal",
+        user="root",
+        password="JXyNzSbNJJHCbVNbcdvZWxyYwvlvFLwN",
+        database="railway",
+        port=3306
+    )
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Users (
+            id INT PRIMARY KEY,
+            rango VARCHAR(255) DEFAULT 'Free user',
+            priv INT DEFAULT 0,
+            dias INT DEFAULT 0,
+            expiracion DATETIME DEFAULT NULL,
+            ban VARCHAR(255) DEFAULT 'No',
+            regist DATETIME DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+    
+    cursor.execute("INSERT IGNORE INTO Users (id, rango, priv, dias, ban, regist) VALUES (%s, 'Free user', 0, 0, 'No', NOW())", (user_id,))
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+    
+    await message.reply_text(regist)
